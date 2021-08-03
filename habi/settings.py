39983fd57c9844b62e901439195e42e3edafedce
serchbauti <perez.sergiob@gmail.com>
2021-08-03 18:30:46 -0500

    feat: add yasg to autodocs

--- habi/settings.py
+++ habi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "drf_yasg",
     "api",
 ]
 
@@ -131,3 +132,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema"}
--- habi/urls.py
+++ habi/urls.py
@@ -14,11 +14,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.urls import include
+from django.urls import path
+from django.urls import re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
 from api import urls as apiUrls
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Habi API",
+        default_version="v1",
+        description="Habi Docs",
+        terms_of_service="",
+        contact=openapi.Contact(email="perez.sergiob@gmail.com"),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(apiUrls)),
+    re_path(
+        r"^doc(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),  # <-- Here
+    path(
+        "doc/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),  # <-- Here
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
+    path("api/", include(apiUrls)),
 ]
