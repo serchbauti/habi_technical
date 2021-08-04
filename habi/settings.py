17f3377afb7111ce6e8c73773c7ba802b48bd181
serchbauti <perez.sergiob@gmail.com>
2021-08-03 20:35:03 -0500

    statics heroku

--- habi/settings.py
+++ habi/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
